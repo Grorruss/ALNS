61c7347ae0d6e3b23ccbac1a5065512379f3f0d9
Alifernu <louis.kuziora@gmail.com>
2020-04-25 17:36:17 +0200

    DevLouis-T2.T4
    Dev feature from Ticket 2 and Ticket 4

---
+++ Projet ALNS/Assets/Script/GameController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+
+    private static float health = 6;
+    private static int maxHealth = 6;
+    private static float moveSpeed = 5f;
+    private static float fireRate = 0.5f;
+    private static float bulletSize = 0.5f;
+
+    public static float Health { get => health; set => health = value; }
+    public static int MaxHealth { get => maxHealth; set => maxHealth = value; }
+    public static float MoveSpeed { get => moveSpeed; set => moveSpeed = value; }
+    public static float FireRate { get => fireRate; set => fireRate = value; }
+    public static float BulletSize { get => bulletSize; set => bulletSize = value; }
+
+    public Text healthText;
+    public Text moveSpeedText;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = "Health: " + health;
+        moveSpeedText.text = "Move Speed: " + MoveSpeed;
+    }
+
+    public static void DamagePlayer(int damage)
+    {
+        health -= damage;
+        if (Health <= 0)
+        {
+            KillPlayer();
+        }
+
+    }
+
+    public static void HealPlayer(float healAmount)
+    {
+        health = Mathf.Min(maxHealth, health + healAmount);
+    }
+
+    public static void MoveSpeedChange(float speed)
+    {
+        moveSpeed += speed;
+    }
+
+    public static void FireRateChange(float rate)
+    {
+        fireRate -= rate;
+    }
+
+    public static void BulletSizeChange(float size)
+    {
+        bulletSize += size;
+    }
+
+    public static void KillPlayer()
+    {
+
+    }
+}
